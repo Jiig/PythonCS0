e722838ed2d95983288a065ecad2b10da8621645
Luke <luktut@gmail.com>
2015-04-01 12:03:27 -0600

    Compound width and height calculating correctly.

--- GObj.py
+++ GObj.py
@@ -100,23 +100,27 @@ class GComp(GObj):
             obj.x = self.x + obj.cx
             obj.y = self.y + obj.cy
             obj.setVisible(self.visible)
+
     def getWidth(self):
         maxX = 0
-        minX = 0
+        minX = 10000
         for obj in self.objs:
-            if obj.x >= maxX:
-                maxX = obj.x
-            elif obj.x <= minX:
-                minX = obj.x
+            min, max = obj._getBox()
+            if max[0] >= maxX:
+                maxX = max[0]
+            if min[0] <= minX:
+                minX = min[0]
         return maxX-minX
+
     def getHeight(self):
         maxY = 0
-        minY = 0
+        minY = 10000
         for obj in self.objs:
-            if obj.y >= maxY:
-                maxY = obj.y
-            elif obj.y <= minY:
-                minY = obj.y
+            min, max = obj._getBox()
+            if max[1] >= maxY:
+                maxY = max[1]
+            if min[1] <= minY:
+                minY = min[1]
         return maxY-minY
 
     def draw(self, window):
@@ -140,19 +144,6 @@ class GComp(GObj):
         maxY = 0
         minX = 10000
         minY = 10000
-        """
-        for obj in self.objs:
-            print obj.x, obj.y
-            if obj.x >= maxX:
-                maxX = obj.x
-            if obj.x <= minX:
-                minX = obj.x
-            if obj.y >= maxY:
-                maxY = obj.y
-            if obj.y <= minY:
-                minY = obj.y
-        return ((minX, minY), (maxX,maxY))
-        """
         for obj in self.objs:
             min, max = obj._getBox()
             #Check min and max X
