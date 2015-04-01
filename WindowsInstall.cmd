0a4d08c3b39e31b9aea21cbc00e833bde0677d2a
Luke <luktut@gmail.com>
2015-04-01 13:00:43 -0600

    Added Sound.py to the installer.

--- WindowsInstall.cmd
+++ WindowsInstall.cmd
@@ -5,3 +5,4 @@ copy Events.py C:\Python33\Lib\Events.py
 copy Globals.py C:\Python33\Lib\Globals.py
 copy GObj.py C:\Python33\Lib\GObj.py
 copy World.py C:\Python33\Lib\World.py
+copy Sound.py C:\Python33\Lib\Sound.py
